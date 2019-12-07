1d3ab5097871eddafedf3da1a1d986bc3484a8b2
pireddag <48107401+pireddag@users.noreply.github.com>
2019-12-07 12:07:25 +0100

    Add files via upload

---
+++ packages/Tables/csvTable.ts
@@ -0,0 +1,23 @@
+<TeXmacs|1.99.11>
+
+<style|source>
+
+<\body>
+  <use-module|(Tables csvTable)>
+
+  <assign|csvTable|<macro|filename|<extern|csvTable|<arg|filename>>>>
+
+  \;
+
+  \;
+
+  \;
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
