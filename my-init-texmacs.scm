f1ae6ebdeaad45e9ad5af71d26a772eb7db2c368
pireddag <pireddag@gmail.com>
2022-03-25 18:58:27 +0100

    Added widget for no-operation cases

--- my-init-texmacs.scm
+++ my-init-texmacs.scm
@@ -3,8 +3,17 @@
 (use-modules (tables simple csvTable)) 
                                        
 
+(tm-widget (table-message-widget)
+  (hlist
+    (text "Please insert table in plain text")
+    >>>
+    (explicit-buttons ("or big-table environment" (display "message from insert table widget")))))
+
 (kbd-map
   ("t a b l e tab"
-   (choose-file insert-csvTable "choose table file" "")))
-
-
+   (if (or (tree-is? (cursor-tree) 'document)
+	   (tree-is? (cursor-tree) 'big-table)
+	   (tree-is? (tree-outer (cursor-tree)) 'document)
+	   (tree-is? (tree-outer (cursor-tree)) 'big-table))
+       (choose-file insert-csvTable "choose table file" "")
+       (top-window table-message-widget "Insert table message"))))
